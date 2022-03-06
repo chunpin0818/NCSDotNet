885ca8239564c20b424cbf4524da669002415f8a
Goh Chun Pin <99855104+chunpin0818@users.noreply.github.com>
2022-03-07 00:33:01 +0800

    Add files via upload

---
+++ C#/Q9. DDL_Import_Attribute/Q9. DDL_Import_Attribute/Program.cs
@@ -0,0 +1,28 @@
+﻿//WAP to implement DLL Import Attribute – Unmanaged dll(user32.dll) to populate a message box  
+//[DLLImport("user32.dll", EntryPoint = "MessageBox")]
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+
+namespace Q9.DDL_Import_Attribute
+{
+    class messageBox
+    {
+        // Import DLL Attribute with Message Box
+        [DllImport("user32.dll", EntryPoint = "MessageBox")]
+        public static extern int MessageBox(IntPtr hWnd, String text, String caption, uint type);
+        //extern = declare method implemented externally
+        //IntPtr = an integer which is same size as a pointer
+        //hWnd = Handle to a window (Part of the Win32 API)
+        //uint = Integral type of value 
+
+        static void Main()
+        {
+            MessageBox(new IntPtr(0), "Hello ! My name is JB :)", "Welcome", 0);
+            Console.ReadKey();
+        }
+    }
+}
