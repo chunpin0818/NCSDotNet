a8098c9f5b0053641ac923cfb3e80751f6d8e62b
Goh Chun Pin <99855104+chunpin0818@users.noreply.github.com>
2022-03-10 01:17:41 +0800

    Add files via upload

---
+++ DocNet/Q1 - ADO.NET_CRUD/Q1 - ADO.NET_CRUD/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Q1___ADO.NET_CRUD
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
