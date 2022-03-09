d6fac9408630afa1d65b88cd68703f3865f2e3b5
Goh Chun Pin <99855104+chunpin0818@users.noreply.github.com>
2022-03-10 01:20:42 +0800

    Add files via upload

---
+++ DocNet/Q2 - Entity_Framework/Q2 - Entity_Framework/Form1.Designer.cs
@@ -0,0 +1,284 @@
+﻿namespace Q2___Entity_Framework
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.adventureWorks2019DataSet = new Q2___Entity_Framework.AdventureWorks2019DataSet();
+            this.adventureWorks2019DataSetBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.adventureWorks2019DataSetBindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.adventureWorks2019DataSetBindingSource2 = new System.Windows.Forms.BindingSource(this.components);
+            this.departmentBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.departmentTableAdapter = new Q2___Entity_Framework.AdventureWorks2019DataSetTableAdapters.DepartmentTableAdapter();
+            this.departmentIDDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.nameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.groupNameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.modifiedDateDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.Insert = new System.Windows.Forms.Button();
+            this.Display = new System.Windows.Forms.Button();
+            this.Update = new System.Windows.Forms.Button();
+            this.Delete = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.adventureWorks2019DataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.adventureWorks2019DataSetBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.adventureWorks2019DataSetBindingSource1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.adventureWorks2019DataSetBindingSource2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.departmentBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.departmentIDDataGridViewTextBoxColumn,
+            this.nameDataGridViewTextBoxColumn,
+            this.groupNameDataGridViewTextBoxColumn,
+            this.modifiedDateDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.departmentBindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(414, 22);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.Size = new System.Drawing.Size(1056, 656);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // adventureWorks2019DataSet
+            // 
+            this.adventureWorks2019DataSet.DataSetName = "AdventureWorks2019DataSet";
+            this.adventureWorks2019DataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // adventureWorks2019DataSetBindingSource
+            // 
+            this.adventureWorks2019DataSetBindingSource.DataSource = this.adventureWorks2019DataSet;
+            this.adventureWorks2019DataSetBindingSource.Position = 0;
+            // 
+            // adventureWorks2019DataSetBindingSource1
+            // 
+            this.adventureWorks2019DataSetBindingSource1.DataSource = this.adventureWorks2019DataSet;
+            this.adventureWorks2019DataSetBindingSource1.Position = 0;
+            // 
+            // adventureWorks2019DataSetBindingSource2
+            // 
+            this.adventureWorks2019DataSetBindingSource2.DataSource = this.adventureWorks2019DataSet;
+            this.adventureWorks2019DataSetBindingSource2.Position = 0;
+            // 
+            // departmentBindingSource
+            // 
+            this.departmentBindingSource.DataMember = "Department";
+            this.departmentBindingSource.DataSource = this.adventureWorks2019DataSetBindingSource2;
+            // 
+            // departmentTableAdapter
+            // 
+            this.departmentTableAdapter.ClearBeforeFill = true;
+            // 
+            // departmentIDDataGridViewTextBoxColumn
+            // 
+            this.departmentIDDataGridViewTextBoxColumn.DataPropertyName = "DepartmentID";
+            this.departmentIDDataGridViewTextBoxColumn.HeaderText = "DepartmentID";
+            this.departmentIDDataGridViewTextBoxColumn.MinimumWidth = 8;
+            this.departmentIDDataGridViewTextBoxColumn.Name = "departmentIDDataGridViewTextBoxColumn";
+            this.departmentIDDataGridViewTextBoxColumn.ReadOnly = true;
+            this.departmentIDDataGridViewTextBoxColumn.Width = 150;
+            // 
+            // nameDataGridViewTextBoxColumn
+            // 
+            this.nameDataGridViewTextBoxColumn.DataPropertyName = "Name";
+            this.nameDataGridViewTextBoxColumn.HeaderText = "Name";
+            this.nameDataGridViewTextBoxColumn.MinimumWidth = 8;
+            this.nameDataGridViewTextBoxColumn.Name = "nameDataGridViewTextBoxColumn";
+            this.nameDataGridViewTextBoxColumn.Width = 150;
+            // 
+            // groupNameDataGridViewTextBoxColumn
+            // 
+            this.groupNameDataGridViewTextBoxColumn.DataPropertyName = "GroupName";
+            this.groupNameDataGridViewTextBoxColumn.HeaderText = "GroupName";
+            this.groupNameDataGridViewTextBoxColumn.MinimumWidth = 8;
+            this.groupNameDataGridViewTextBoxColumn.Name = "groupNameDataGridViewTextBoxColumn";
+            this.groupNameDataGridViewTextBoxColumn.Width = 150;
+            // 
+            // modifiedDateDataGridViewTextBoxColumn
+            // 
+            this.modifiedDateDataGridViewTextBoxColumn.DataPropertyName = "ModifiedDate";
+            this.modifiedDateDataGridViewTextBoxColumn.HeaderText = "ModifiedDate";
+            this.modifiedDateDataGridViewTextBoxColumn.MinimumWidth = 8;
+            this.modifiedDateDataGridViewTextBoxColumn.Name = "modifiedDateDataGridViewTextBoxColumn";
+            this.modifiedDateDataGridViewTextBoxColumn.Width = 150;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(36, 57);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(111, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "DepartmentID";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(36, 139);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(51, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(36, 219);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(96, 20);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "GroupName";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(40, 80);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(258, 26);
+            this.textBox1.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(40, 162);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(258, 26);
+            this.textBox2.TabIndex = 6;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(40, 242);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(258, 26);
+            this.textBox3.TabIndex = 7;
+            // 
+            // Insert
+            // 
+            this.Insert.Location = new System.Drawing.Point(32, 444);
+            this.Insert.Name = "Insert";
+            this.Insert.Size = new System.Drawing.Size(137, 66);
+            this.Insert.TabIndex = 9;
+            this.Insert.Text = "Insert";
+            this.Insert.UseVisualStyleBackColor = true;
+            this.Insert.Click += new System.EventHandler(this.Insert_Click);
+            // 
+            // Display
+            // 
+            this.Display.Location = new System.Drawing.Point(214, 444);
+            this.Display.Name = "Display";
+            this.Display.Size = new System.Drawing.Size(137, 63);
+            this.Display.TabIndex = 10;
+            this.Display.Text = "Display";
+            this.Display.UseVisualStyleBackColor = true;
+            this.Display.Click += new System.EventHandler(this.Display_Click);
+            // 
+            // Update
+            // 
+            this.Update.Location = new System.Drawing.Point(32, 527);
+            this.Update.Name = "Update";
+            this.Update.Size = new System.Drawing.Size(137, 67);
+            this.Update.TabIndex = 11;
+            this.Update.Text = "Update";
+            this.Update.UseVisualStyleBackColor = true;
+            this.Update.Click += new System.EventHandler(this.Update_Click);
+            // 
+            // Delete
+            // 
+            this.Delete.Location = new System.Drawing.Point(214, 527);
+            this.Delete.Name = "Delete";
+            this.Delete.Size = new System.Drawing.Size(137, 67);
+            this.Delete.TabIndex = 12;
+            this.Delete.Text = "Delete";
+            this.Delete.UseVisualStyleBackColor = true;
+            this.Delete.Click += new System.EventHandler(this.Delete_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1492, 706);
+            this.Controls.Add(this.Delete);
+            this.Controls.Add(this.Update);
+            this.Controls.Add(this.Display);
+            this.Controls.Add(this.Insert);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.adventureWorks2019DataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.adventureWorks2019DataSetBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.adventureWorks2019DataSetBindingSource1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.adventureWorks2019DataSetBindingSource2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.departmentBindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.BindingSource adventureWorks2019DataSetBindingSource2;
+        private AdventureWorks2019DataSet adventureWorks2019DataSet;
+        private System.Windows.Forms.BindingSource adventureWorks2019DataSetBindingSource;
+        private System.Windows.Forms.BindingSource adventureWorks2019DataSetBindingSource1;
+        private System.Windows.Forms.BindingSource departmentBindingSource;
+        private AdventureWorks2019DataSetTableAdapters.DepartmentTableAdapter departmentTableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn departmentIDDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nameDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn groupNameDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn modifiedDateDataGridViewTextBoxColumn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button Insert;
+        private System.Windows.Forms.Button Display;
+        private System.Windows.Forms.Button Update;
+        private System.Windows.Forms.Button Delete;
+    }
+}
+
---
+++ DocNet/Q2 - Entity_Framework/Q2 - Entity_Framework/Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Q2___Entity_Framework
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'adventureWorks2019DataSet.Department' table. You can move, or remove it, as needed.
+            SetDataInGridView();
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void Insert_Click(object sender, EventArgs e)
+        {
+            DateTime dt=DateTime.Now;
+            Department dp = new Department();
+            dp.DepartmentID = Convert.ToInt16(textBox1.Text);
+            dp.Name = textBox2.Text.Trim();
+            dp.GroupName = textBox3.Text.Trim();
+            dp.ModifiedDate = dt;
+
+            try
+            {
+                AdventureWorks2019Entities db = new AdventureWorks2019Entities();
+                db.Departments.Add(dp);
+                db.SaveChanges();
+                SetDataInGridView();
+                MessageBox.Show("Records are Saved Successfully");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Not Saved \nReason:" + ex);
+                SetDataInGridView();
+            }
+            
+        }
+
+        private void Display_Click(object sender, EventArgs e)
+        {
+            SetDataInGridView();
+        }
+
+        private void Update_Click(object sender, EventArgs e)
+        {
+            DateTime dt = DateTime.Now;
+            Department dp = new Department();
+            short updated_ID = Convert.ToInt16(textBox1.Text);
+            string Name = textBox2.Text.Trim();
+            string GroupName = textBox3.Text.Trim();
+
+            try
+            {
+                AdventureWorks2019Entities db = new AdventureWorks2019Entities();
+                dp = db.Departments.Where(d => d.DepartmentID == updated_ID).First();
+                dp.Name = Name;
+                dp.GroupName = GroupName;
+                dp.ModifiedDate = dt;
+                db.SaveChanges();
+                SetDataInGridView();
+                MessageBox.Show("Records are Updated Successfully");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Not Updated \nReason:" + ex);
+                SetDataInGridView();
+            }               
+        }
+
+        private void Delete_Click(object sender, EventArgs e)
+        {
+            DateTime dt = DateTime.Now;
+            Department dp = new Department();
+            short deleted_ID = Convert.ToInt16(textBox1.Text);
+            
+            try
+            {
+                AdventureWorks2019Entities db = new AdventureWorks2019Entities();
+                dp = db.Departments.Where(d => d.DepartmentID == deleted_ID).First();
+                db.Departments.Remove(dp);
+                db.SaveChanges();
+                SetDataInGridView();
+                MessageBox.Show("Records are Deleted Successfully");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Not Deleted \nReason:"+ex);
+                SetDataInGridView();
+            }
+            
+        }
+
+        public void SetDataInGridView()
+        {
+            AdventureWorks2019Entities db = new AdventureWorks2019Entities();
+            dataGridView1.AutoGenerateColumns = false; 
+            dataGridView1.DataSource = db.Departments.ToList<Department>();
+        }
+
+    }
+}
